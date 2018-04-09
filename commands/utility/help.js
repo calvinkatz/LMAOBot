e4602cc321b9fb876490ebff43448cd8cf60fc8a
sirpete <dylohowes@gmail.com>
2018-04-09 23:44:08 +1000

    fix af

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -3,7 +3,7 @@ module.exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setTitle("Available Commands")
     .setDescription("The prefix for my commands is `lmao`, so using a command would look like this: `lmao (command)`")
-    .addField(":loud_sound: Sound Commands", "`addsound`, `listsounds`, `listpopularsounds`, `playsound`, `soundinfo`")
+    .addField(":loud_sound: Sound Commands", "`addsound`, `listsounds`, `listpopularsounds`, `playsound`, `soundinfo`, `mysounds`, `deletesound`")
     .addField(":laughing: Fun Commands", "`agree`, `dankrate`, `gayrate`, `8ball`, `pun`, `roll`, `coinflip`, `ask`, `chuck`, `say`, `magik`")
     .addField(":camera: Image Commands", "`alone`, `doge`, `dolan`, `kappa`, `lenny`, `lol`, `megusta`, `notsure`, `pepe`, `sanic`, `spiderman`, `spooderman`, `troll`, `wat`")
     .addField(":shield: Utility Commands", "`donate`, `help`, `info`, `invite`, `ping`, `upvote`")

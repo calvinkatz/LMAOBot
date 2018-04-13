f565f23c234503cddc9fb15b9d79d1172db28eac
sirpete <dylohowes@gmail.com>
2018-04-14 02:47:19 +1000

    fix

--- bot.js
+++ bot.js
@@ -253,24 +253,9 @@ client.on('message', async msg => {
   }
 });
 
-client.on('userUpdate', async () => {
-  try {
-    const updateuser = await userInfo.update({ username: `${msg.author.username}#${msg.author.discriminator}` }, { where: { id: msg.author.id } });
-  } catch (err) {
-    if(err.name == "ReferenceError: msg is not defined") {
-      try {
-        const userinf = await userInfo.create({
-          id: msg.author.id,
-          user_name: `${msg.author.username}#${msg.author.discriminator}`,
-        });
-        
-      } catch (error) {
-        if (error.name !== 'SequelizeUniqueConstraintError') console.log(`Got an error: ${err}`);
-      }
-    }
-    console.log(`An error occured: ${err}`);
-  }
-});
+// client.on('userUpdate', async () => {
+
+// });
 
 client.on('guildCreate', guild => {
   // Get channel in which bot is allowed to msg

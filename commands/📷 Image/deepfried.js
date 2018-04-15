3819ce79e09b902a40d9500343cc753ce3d47ad9
Christian Cuevas <ccpotential@gmail.com>
2018-04-15 13:20:02 -0700

    turn back
    Clark was here ;V

--- commands/📷 Image/deepfried.js
+++ commands/📷 Image/deepfried.js
@@ -1,56 +1,58 @@
 const https = require('https');
+const url = 'https://www.reddit.com/r/deepfriedmemes/hot/.json?limit=100';
+const Discord = require('discord.js');
 
 module.exports = {
   // Information
   name: 'deepfried',
   aliases: ['df'],
   description: 'Get a random meme from Reddit/deepfried.',
+
   // Function
-  run(client, command, msg, args) {
-    https.get('https://www.reddit.com/r/deepfriedmemes/random/.json?limit=1', function(res) {
+  run: (client, command, msg, args) => {
+    https.get(url, (res) => {
       let body = '';
 
       res.on('data', (chunk) => {
         body += chunk;
       });
 
-      res.on('end', async function() {
-        const res = JSON.parse(body);
-
+      res.on('end', () => {
+        const response = JSON.parse(body);
+        const index = response.data.children[Math.floor(Math.random() * 99) + 1].data;
         if (index.post_hint !== 'image') {
           var text = index.selftext;
-          const embed = new Discord.RichEmbed()
+          const textembed = new Discord.RichEmbed()
             .setTitle(subredditname)
             .setColor(0x00AE86)
             .setDescription(`[${title}](${link_to_post})\n\n${text}`)
             .setURL(`https://reddit.com/${subredditname}`);
-        } else if (index.post_hint !== 'image') {
-          const image = index.preview.images[0].source.url;
-          var title = index.title;
-          var link_to_post = 'https://reddit.com' + index.permalink;
-          var subredditname = index.subreddit_name_prefixed;
-          const embed = new Discord.RichEmbed()
-            .setTitle(subredditname)
-            .setColor(0x00AE86)
-            .setDescription(`[${title}](${link_to_post})\n\n${text}`)
-            .setURL(`https://reddit.com/${subredditname}`);
-        } else {
-          const embed = new Discord.RichEmbed()
-            .setTitle(subredditname)
-            .setImage(image)
-            .setColor(0x00AE86)
-            .setDescription(`[${title}](${link_to_post})`)
-            .setURL(`https://reddit.com/${subredditname}`);
-        }
-        reply = await msg.channel.send(embed);
 
-        await reply.react('🔃');
+          msg.channel.send(textembed);
+        }
+        const image = index.preview.images[0].source.url;
+        var title = index.title;
+        var link_to_post = 'https://reddit.com' + index.permalink;
+        var subredditname = index.subreddit_name_prefixed;
+        if (index.post_hint !== 'image') {
+          const textembed = new Discord.RichEmbed()
+            .setTitle(subredditname)
+            .setColor(0x00AE86)
+            .setDescription(`[${title}](${link_to_post})\n\n${text}`)
+            .setURL(`https://reddit.com/${subredditname}`);
+
+          msg.channel.send(textembed);
+        }
+        const imageembed = new Discord.RichEmbed()
+          .setTitle(subredditname)
+          .setImage(image)
+          .setColor(0x00AE86)
+          .setDescription(`[${title}](${link_to_post})`)
+          .setURL(`https://reddit.com/${subredditname}`);
+        msg.channel.send(imageembed);
       }).on('error', function(e) {
         console.log('Got an error: ', e);
       });
     });
   },
-  async on_reaction(bot, msg, type, reaction) {
-
-  }
 };

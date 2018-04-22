efbf564173700dd9da9c4f479584cf3041e965c4
NumerX <30758831+NumerX@users.noreply.github.com>
2018-04-22 12:06:26 +0200

    removed gdocs and added #feedback channel system
    you can manage the submission using a reaction-based system

--- commands/🛡 Utility/feedback.js
+++ commands/🛡 Utility/feedback.js
@@ -1,52 +1,56 @@
-const {google} = require('googleapis')
-const sheets = google.sheets('v4')
-
-require("dotenv").config(); // this is for windows only. REMOVE WHEN UPLOADING TO VPS!!!!!4!!!!
-
 module.exports = {
   // Information
   name: 'feedback',
   aliases: ['fb'],
-  description: 'Give the bot developers feedback!',
+  description: 'Give the developers some feedback!',
   args: {
     req: true,
     min: 1,
   },
-  cooldown: 60,
-  // Function
-  run: (client, command, message, args) => {
-    const oauth2Client = new google.auth.OAuth2(
-      process.env.GOOGLE_ID,
-      process.env.GOOGLE_SECRET
-    );
-
-    oauth2Client.credentials = {
-      refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-    };
-
-    oauth2Client.refreshAccessToken((err, tokens) => {
-    if (err) return message.channel.send("An exception occured! Please try again later.").then(console.error(err));
-
-    oauth2Client.credentials = tokens;
-
-    sheets.spreadsheets.values.append({
-      spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
-      range: 'Sheet1',
-      valueInputOption: 'USER_ENTERED',
-      insertDataOption: 'INSERT_ROWS',
-      resource: {
-        values: [
-          [new Date().toISOString(), message.author.id, message.author.tag, args.join(" "), '=\"#\"&Row()-5']   
-        ],
+  cooldown: 300,
+  embed: (msg, client, colour, args, state) => {
+    return {
+      color: colour,
+      author: {
+        name: msg.author.username,
+        icon_url: msg.author.avatarURL
       },
-      auth: oauth2Client
-    }, (err, response) => {
-      if (err) return message.channel.send(":x: An exception occured! Try again in a moment!");
-    });
-
-    message.channel.send(`${client.config.emojis.info} Thank you! We have received your feedback.
-**PLEASE NOTE**: Abusing this command will result in a permanent ban from ever using it again!`)
-
-  })
+      title: `${msg.author.tag} (${msg.author.id})`,
+      description: args.join(' '),
+      timestamp: new Date(),
+      footer: {
+       icon_url: client.user.avatarURL,
+       text: client.config.embed.footer,
+     }
+   }
+ },
+  // Function
+  run: async (client, command, msg, args) => {
+    try {
+      const fbchannel = client.guilds.get(client.config.support.id).channels.get(client.config.support.feedback);
+      const embed = await fbchannel.send({
+        embed: command.embed(msg, client, 0x0000ff, args)
+      })
+      msg.channel.send(`${client.config.emojis.info} Thank you! We have received your feedback. \n**PLEASE NOTE**: Abusing this command will result in a permanent ban from ever using it again!`) 
+      await client.add_msg_reaction_listener(command, embed, ['👍', '👎'], {
+        extra: {
+          args: args,
+          msg: msg
+        },
+      }); 
+    } catch (err) {
+      msg.channel.send(":x: Uh oh! Something went wrong. Try again later.");
+      console.error(`Feedback.js ERROR: ${err}`)
+    }
+    
+  },
+  on_reaction: (client, command, data, operation, reaction) => {
+    if(reaction.emoji.name === "👍") {
+      data.message.edit({embed: command.embed(data.extra.msg, client, 0x00ff00, data.extra.args)})
+      data.message.pin()
+    } else if(reaction.emoji.name == "👎") {
+      data.message.edit({embed: command.embed(data.extra.msg, client, 0xff0000, data.extra.args)})
+      if(data.message.pinned) data.message.unpin()
+    }
   }
 };

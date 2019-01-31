43ac8a9d3e0d299d7cc71b3839eb728940b6865c
Calvin Kohler-Katz <calvinkatz@gmail.com>
2019-01-31 14:50:42 -0600

    Updated Image commands to match DankMeme

--- commands/Image/deepfried.js
+++ commands/Image/deepfried.js
@@ -3,56 +3,58 @@ const url = 'https://www.reddit.com/r/deepfriedmemes/hot/.json?limit=100';
 const Discord = require('discord.js');
 
 module.exports = {
-  // Information
-  name: 'deepfried',
-  aliases: ['df'],
-  description: 'Get a random meme from Reddit/deepfried.',
+	// Information
+	name: 'deepfried',
+	aliases: ['df'],
+	description: 'Get a random meme from Reddit/deepfried.',
 
-  // Function
-  run: (client, command, msg, args) => {
-    https.get(url, (res) => {
-      let body = '';
+    // Function
+    run: (client, command, msg, args) => {
+        https.get(url, (res) => {
+            var body = '';
 
-      res.on('data', (chunk) => {
-        body += chunk;
-      });
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
 
-      res.on('end', () => {
-        const response = JSON.parse(body);
-        const index = response.data.children[Math.floor(Math.random() * 99) + 1].data;
-        if (index.post_hint !== 'image') {
-          var text = index.selftext;
-          const textembed = new Discord.RichEmbed()
-            .setTitle(subredditname)
-            .setColor(0x00AE86)
-            .setDescription(`[${title}](${link_to_post})\n\n${text}`)
-            .setURL(`https://reddit.com/${subredditname}`);
+            res.on('end', () => {
+                var response = JSON.parse(body);
+                var index = null;
+                var counter = 0;
+                do {
+                    counter++;
+                    index = response.data.children[Math.floor(Math.random() * 99) + 1].data;
+                }
+                while (index.post_hint != "image" && counter < 10)
 
-          msg.channel.send(textembed);
-        }
-        const image = index.preview.images[0].source.url;
-        var title = index.title;
-        var link_to_post = 'https://reddit.com' + index.permalink;
-        var subredditname = index.subreddit_name_prefixed;
-        if (index.post_hint !== 'image') {
-          const textembed = new Discord.RichEmbed()
-            .setTitle(subredditname)
-            .setColor(0x00AE86)
-            .setDescription(`[${title}](${link_to_post})\n\n${text}`)
-            .setURL(`https://reddit.com/${subredditname}`);
+                var title = index.title;
+                var link_to_post = "https://reddit.com" + index.permalink;
+                var subredditname = index.subreddit_name_prefixed;
 
-          msg.channel.send(textembed);
-        }
-        const imageembed = new Discord.RichEmbed()
-          .setTitle(subredditname)
-          .setImage(image)
-          .setColor(0x00AE86)
-          .setDescription(`[${title}](${link_to_post})`)
-          .setURL(`https://reddit.com/${subredditname}`);
-        msg.channel.send(imageembed);
-      }).on('error', function (e) {
-        console.log('Got an error: ', e);
-      });
-    });
-  },
-};
+                if (counter >= 10) {
+                    var text = index.selftext;
+                    const textembed = new Discord.RichEmbed()
+                        .setTitle(subredditname)
+                        .setColor(0x00AE86)
+                        .setDescription(`[${title}](${link_to_post})\n\n${text}`)
+                        .setURL(`https://reddit.com/${subredditname}`);
+
+                    msg.channel.send(textembed);
+                }
+                else {
+                    var image = index.url
+                    const imageembed = new Discord.RichEmbed()
+                        .setTitle(subredditname)
+                        .setImage(image)
+                        .setColor(0x00AE86)
+                        .setDescription(`[${title}](${link_to_post})`)
+                        .setURL(`https://reddit.com/${subredditname}`);
+
+                    msg.channel.send(imageembed);
+                }
+            }).on('error', function (e) {
+                console.log("Got an error: ", e);
+            })
+        });
+    }
+}
--- commands/Image/reddit.js
+++ commands/Image/reddit.js
@@ -1,212 +1,212 @@
 const rp = require('request-promise');
 
 module.exports = {
-  // Information
-  name: 'reddit',
-  description: 'Browse through Reddit via Discord.',
-  usage: '<subreddit> <view>',
-  explanation: {
-    subreddit: {
-      description: 'Subreddit to pull post from.',
-      default: 'random',
-    },
-    view: {
-      description: 'What sort of post to view.',
-      default: 'hot',
-      options: ['hot', 'top', 'new', 'rising', 'controversial'],
-    },
-  },
-  aliases: ['re'],
-  // Requirements
-  // Custom Data
-  subreddits: [
-    'dankmemes',
-    'fffffffuuuuuuuuuuuu',
-    'vertical',
-    'AdviceAnimals',
-    'Inglip',
-    'wholesomememes',
-    'MemeEconomy',
-    'BlackPeopleTwitter',
-    'shittyadviceanimals',
-    'ProgrammerHumor',
-    'oddlysatisfying',
-    'mildlyinfuriating',
-    'softwaregore',
-    'me_irl',
-    'mildlyinteresting',
-  ],
-  // Function
-  run: async (client, command, msg, args) => {
-    let subreddit = command.subreddits[Math.floor(Math.random() * command.subreddits.length)];
-    if (args.length > 0) {
-      subreddit = args[0].toLowerCase();
-    }
+	// Information
+	name: 'reddit',
+	description: 'Browse through Reddit via Discord.',
+	usage: '<subreddit> <view>',
+	explanation: {
+		subreddit: {
+			description: 'Subreddit to pull post from.',
+			default: 'random',
+		},
+		view: {
+			description: 'What sort of post to view.',
+			default: 'hot',
+			options: ['hot', 'top', 'new', 'rising', 'controversial'],
+		},
+	},
+	aliases: ['re'],
+	// Requirements
+	// Custom Data
+	subreddits: [
+		'dankmemes',
+		'fffffffuuuuuuuuuuuu',
+		'vertical',
+		'AdviceAnimals',
+		'Inglip',
+		'wholesomememes',
+		'MemeEconomy',
+		'BlackPeopleTwitter',
+		'shittyadviceanimals',
+		'ProgrammerHumor',
+		'oddlysatisfying',
+		'mildlyinfuriating',
+		'softwaregore',
+		'me_irl',
+		'mildlyinteresting',
+	],
+	// Function
+	run: async (client, command, msg, args) => {
+		let subreddit = command.subreddits[Math.floor(Math.random() * command.subreddits.length)];
+		if (args.length > 0) {
+			subreddit = args[0].toLowerCase();
+		}
 
-    let endpoint = 'hot';
-    if (args.length >= 2 && command.explanation.view.options.includes(args[1].toLowerCase())) {
-      endpoint = args[1].toLowerCase();
-    }
+		let endpoint = 'hot';
+		if (args.length >= 2 && command.explanation.view.options.includes(args[1].toLowerCase())) {
+			endpoint = args[1].toLowerCase();
+		}
 
-    const url = `https://www.reddit.com/r/${subreddit}/${endpoint}/`;
+		const url = `https://www.reddit.com/r/${subreddit}/${endpoint}/`;
 
-    let response;
-    try {
-      response = await rp(`https://www.reddit.com/r/${subreddit}/${endpoint}.json?limit=100`);
-    } catch (error) {
-      return msg.channel.send({
-        embed: {
-          color: 0x00AE86,
-          author: {
-            name: client.user.username,
-            icon_url: client.user.avatarURL,
-          },
-          title: `r/${subreddit}/${endpoint}`,
-          description: 'Subreddit was not found :confused:',
-          timestamp: new Date(),
-          footer: {
-            icon_url: client.user.avatarURL,
-            text: client.config.embed.footer,
-          },
-        },
-      });
-    }
+		let response;
+		try {
+			response = await rp(`https://www.reddit.com/r/${subreddit}/${endpoint}.json?limit=100`);
+		} catch (error) {
+			return msg.channel.send({
+				embed: {
+					color: 0x00AE86,
+					author: {
+						name: client.user.username,
+						icon_url: client.user.avatarURL,
+					},
+					title: `r/${subreddit}/${endpoint}`,
+					description: 'Subreddit was not found :confused:',
+					timestamp: new Date(),
+					footer: {
+						icon_url: client.user.avatarURL,
+						text: client.config.embed.footer,
+					},
+				},
+			});
+		}
 
-    const posts = JSON.parse(response).data.children;
+		const posts = JSON.parse(response).data.children;
 
-    if (posts.length === 0) {
-      return msg.channel.send({
-        embed: {
-          color: 0x00AE86,
-          author: {
-            name: client.user.username,
-            icon_url: client.user.avatarURL,
-          },
-          title: `r/${subreddit}/${endpoint}`,
-          description: 'Nothing was found :confused:',
-          timestamp: new Date(),
-          footer: {
-            icon_url: client.user.avatarURL,
-            text: client.config.embed.footer,
-          },
-        },
-      });
-    }
+		if (posts.length === 0) {
+			return msg.channel.send({
+				embed: {
+					color: 0x00AE86,
+					author: {
+						name: client.user.username,
+						icon_url: client.user.avatarURL,
+					},
+					title: `r/${subreddit}/${endpoint}`,
+					description: 'Nothing was found :confused:',
+					timestamp: new Date(),
+					footer: {
+						icon_url: client.user.avatarURL,
+						text: client.config.embed.footer,
+					},
+				},
+			});
+		}
 
-    let index = 0;
-    let checks = 0;
-    do {
-      if (checks >= 100) {
-        return msg.channel.send({
-          embed: {
-            color: 0x00AE86,
-            author: {
-              name: client.user.username,
-              icon_url: client.user.avatarURL,
-            },
-            title: `r/${subreddit}/${endpoint}`,
-            description: 'Nothing was found :confused:',
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL,
-              text: client.config.embed.footer,
-            },
-          },
-        });
-      }
-      checks += 1;
+		let index = 0;
+		let checks = 0;
+		do {
+			if (checks >= 100) {
+				return msg.channel.send({
+					embed: {
+						color: 0x00AE86,
+						author: {
+							name: client.user.username,
+							icon_url: client.user.avatarURL,
+						},
+						title: `r/${subreddit}/${endpoint}`,
+						description: 'Nothing was found :confused:',
+						timestamp: new Date(),
+						footer: {
+							icon_url: client.user.avatarURL,
+							text: client.config.embed.footer,
+						},
+					},
+				});
+			}
+			checks += 1;
 
-      index = Math.floor(Math.random() * posts.length);
+			index = Math.floor(Math.random() * posts.length);
 
-      if (index < 0) {
-        index = posts.length - 1;
-      } else if (index > (posts.length - 1)) {
-        index = 0;
-      }
-    } while (posts[index].data.over_18 || !('preview' in posts[index].data) || posts[index].data.preview.images.length === 0);
+			if (index < 0) {
+				index = posts.length - 1;
+			} else if (index > (posts.length - 1)) {
+				index = 0;
+			}
+		} while (posts[index].data.over_18 || !('preview' in posts[index].data) || posts[index].data.preview.images.length === 0);
 
-    const reply = await msg.channel.send({
-      embed: command.post_embed(client, posts[index].data, {
-        url: url,
-        subreddit: subreddit,
-        endpoint: endpoint,
-      }),
-    });
+		const reply = await msg.channel.send({
+			embed: command.post_embed(client, posts[index].data, {
+				url: url,
+				subreddit: subreddit,
+				endpoint: endpoint,
+			}),
+		});
 
-    await client.add_msg_reaction_listener(command, reply, ['⬅', '🔄', '➡'], {
-      extra: {
-        url: url,
-        subreddit: subreddit,
-        endpoint: endpoint,
-        index: index,
-        posts: posts,
-      },
-    });
-  },
-  post_embed: (client, post, data) => {
-    return {
-      color: 0x00AE86,
-      author: {
-        name: client.user.username,
-        icon_url: client.user.avatarURL,
-      },
-      title: `r/${data.subreddit}/${data.endpoint}`,
-      url: data.url,
-      description: `[${post.title}](https://reddit.com${post.permalink})\n${post.selftext}`,
-      image: {
-        url: post.preview.images[0].source.url,
-      },
-      timestamp: new Date(),
-      footer: {
-        icon_url: client.user.avatarURL,
-        text: client.config.embed.footer,
-      },
-    };
-  },
-  on_reaction: (client, command, data, operation, reaction) => {
-    let checks = 0;
-    do {
-      if (checks >= 100) {
-        return data.message.edit({
-          embed: {
-            color: 0x00AE86,
-            author: {
-              name: client.user.username,
-              icon_url: client.user.avatarURL,
-            },
-            title: `r/${data.extra.subreddit}/${data.extra.endpoint}`,
-            description: 'Nothing was found :confused:',
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL,
-              text: client.config.embed.footer,
-            },
-          },
-        });
-      }
-      checks += 1;
+		await client.add_msg_reaction_listener(command, reply, ['⬅', '🔄', '➡'], {
+			extra: {
+				url: url,
+				subreddit: subreddit,
+				endpoint: endpoint,
+				index: index,
+				posts: posts,
+			},
+		});
+	},
+	post_embed: (client, post, data) => {
+		return {
+			color: 0x00AE86,
+			author: {
+				name: client.user.username,
+				icon_url: client.user.avatarURL,
+			},
+			title: `r/${data.subreddit}/${data.endpoint}`,
+			url: data.url,
+			description: `[${post.title}](https://reddit.com${post.permalink})\n${post.selftext}`,
+			image: {
+				url: post.url,
+			},
+			timestamp: new Date(),
+			footer: {
+				icon_url: client.user.avatarURL,
+				text: client.config.embed.footer,
+			},
+		};
+	},
+	on_reaction: (client, command, data, operation, reaction) => {
+		let checks = 0;
+		do {
+			if (checks >= 100) {
+				return data.message.edit({
+					embed: {
+						color: 0x00AE86,
+						author: {
+							name: client.user.username,
+							icon_url: client.user.avatarURL,
+						},
+						title: `r/${data.extra.subreddit}/${data.extra.endpoint}`,
+						description: 'Nothing was found :confused:',
+						timestamp: new Date(),
+						footer: {
+							icon_url: client.user.avatarURL,
+							text: client.config.embed.footer,
+						},
+					},
+				});
+			}
+			checks += 1;
 
-      if (reaction.emoji.name === '⬅') {
-        data.extra.index -= 1;
-      } else if (reaction.emoji.name === '🔄') {
-        data.extra.index = Math.floor(Math.random() * data.extra.posts.length);
-      } else if (reaction.emoji.name === '➡') {
-        data.extra.index += 1;
-      }
+			if (reaction.emoji.name === '⬅') {
+				data.extra.index -= 1;
+			} else if (reaction.emoji.name === '🔄') {
+				data.extra.index = Math.floor(Math.random() * data.extra.posts.length);
+			} else if (reaction.emoji.name === '➡') {
+				data.extra.index += 1;
+			}
 
-      if (data.extra.index < 0) {
-        data.extra.index = data.extra.posts.length - 1;
-      } else if (data.extra.index > (data.extra.posts.length - 1)) {
-        data.extra.index = 0;
-      }
-    } while (data.extra.posts[data.extra.index].data.over_18 || !('preview' in data.extra.posts[data.extra.index].data) || data.extra.posts[data.extra.index].data.preview.images.length === 0);
+			if (data.extra.index < 0) {
+				data.extra.index = data.extra.posts.length - 1;
+			} else if (data.extra.index > (data.extra.posts.length - 1)) {
+				data.extra.index = 0;
+			}
+		} while (data.extra.posts[data.extra.index].data.over_18 || !('preview' in data.extra.posts[data.extra.index].data) || data.extra.posts[data.extra.index].data.preview.images.length === 0);
 
-    data.message.edit({
-      embed: command.post_embed(client, data.extra.posts[data.extra.index].data, {
-        url: data.extra.url,
-        subreddit: data.extra.subreddit,
-        endpoint: data.extra.endpoint,
-      }),
-    });
-  },
+		data.message.edit({
+			embed: command.post_embed(client, data.extra.posts[data.extra.index].data, {
+				url: data.extra.url,
+				subreddit: data.extra.subreddit,
+				endpoint: data.extra.endpoint,
+			}),
+		});
+	},
 };
